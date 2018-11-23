8c15bddb0f8e473a3f748c749cf6ad613d51c8e7
LiuTingWed <1991261508@qq.com>
2018-11-23 19:57:29 +0800

    添加动态培训功能

---
+++ src/main/java/com/trainingcenter/controller/TrainingDynamicController.java
@@ -0,0 +1,198 @@
+package com.trainingcenter.controller;
+
+import com.trainingcenter.bean.TrainingDynamic;
+import com.trainingcenter.bean.User;
+import com.trainingcenter.controller.validation.TC_Update;
+import com.trainingcenter.exception.UpdateException;
+import com.trainingcenter.service.TrainingDynamicService;
+import com.trainingcenter.service.UserService;
+import com.trainingcenter.utils.AjaxJson;
+import com.trainingcenter.utils.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static com.trainingcenter.utils.SysResourcesUtils.getCurrentUsername;
+
+/**
+ * @author Liutingwei
+ * @date 2018-11-9 19:45
+ */
+@RequestMapping("/trainingDynamic")
+@Controller
+public class TrainingDynamicController {
+
+
+    @Qualifier("trainingDynamicService")
+    @Autowired
+    private TrainingDynamicService trainingDynamicService;
+
+    @Qualifier("userService")
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/listPage",method = RequestMethod.GET)
+    @ResponseBody
+    public AjaxJson listPage(@RequestParam("currentPage") Integer currentPage, @RequestParam("rows") Integer rows, String searchContent){
+        AjaxJson ajaxJson = new AjaxJson();
+        if(currentPage < 0 || rows < 0 ){
+            return null;
+        }
+        List<TrainingDynamic> trainingDynamics = trainingDynamicService.getTrainingDynamics(currentPage, rows, searchContent);
+
+        if(trainingDynamics.size() == 0){
+            ajaxJson.setCode(1);
+            ajaxJson.setMsg("暂无数据");
+            return ajaxJson;
+        }
+
+        Map<String, Object> map = new ConcurrentHashMap<>();
+        map.put("trainingDynamic",trainingDynamics);
+        ajaxJson.setData(map);
+        return ajaxJson;
+    }
+
+    /**
+     * 进入详细页面
+     *
+     * @return loginPage
+     */
+    @RequestMapping(value = "/detailsPage", method = RequestMethod.GET)
+    public ModelAndView detailsPage(@RequestParam("id") String id) {
+        String viewName = "static/detailsPage";
+        return new ModelAndView(viewName);
+    }
+
+    /**
+     * 进入添加页面
+     *
+     * @return loginPage
+     */
+    @RequestMapping(value = "/addPage", method = RequestMethod.GET)
+    public ModelAndView addPage() {
+        String viewName = "static/addPage";
+        return new ModelAndView(viewName);
+    }
+
+    /**
+     * 进入更新页面
+     *
+     * @return loginPage
+     */
+    @RequestMapping(value = "/updatePage", method = RequestMethod.GET)
+    public ModelAndView updatePage(HttpServletRequest request) {
+        String id = request.getParameter("trainingDynamicId");
+        ModelAndView modelAndView =new ModelAndView();
+        if(StringUtil.isNotEmpty(id)){
+            String viewName = "static/updatePage";
+            modelAndView.addObject("trainingDynamic",trainingDynamicService.getTrainingDynamicById(id));
+            modelAndView.setViewName(viewName);
+
+        }
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/update",method = RequestMethod.GET)
+    @ResponseBody
+    public AjaxJson update(@Validated(value = {TC_Update.class}) TrainingDynamic trainingDynamic){
+        AjaxJson ajaxJson = new AjaxJson();
+
+        //获取当前用户
+        String currentName = getCurrentUsername();
+        User user = userService.getUserByUsername(currentName);
+
+        if (user == null){
+            throw new CredentialsExpiredException("登录凭证已过期");
+        }
+
+        if (trainingDynamic == null){
+            ajaxJson.setCode(0);
+            ajaxJson.setMsg("操作失败，对象不能为空");
+            return ajaxJson;
+
+        }
+        //更新操作
+        TrainingDynamic newTD = trainingDynamicService.getTrainingDynamicById(trainingDynamic.getId());
+        if (newTD == null){
+            throw new UpdateException("更新失败，对象不存在或已被删除");
+        }
+        newTD.setTitle(trainingDynamic.getTitle());
+        newTD.setRemarks(trainingDynamic.getRemarks());
+        newTD.setContent(trainingDynamic.getContent());
+        newTD.setImgs(trainingDynamic.getImgs());
+        newTD.setUpdateDate(new Date());
+        newTD.setUpdateUserId(user.getId());
+
+        return ajaxJson;
+    }
+
+
+    public AjaxJson add(@RequestParam("title") String title, @RequestParam("imgs") String imgs
+            , @RequestParam("content") String content, String remarks){
+        AjaxJson ajaxJson = new AjaxJson();
+
+        //获取当前用户
+        String currentName = getCurrentUsername();
+
+        User user = userService.getUserByUsername(currentName);
+
+        //非空验证
+        if (StringUtil.isEmpty(title)) {
+            ajaxJson.setCode(0);
+            ajaxJson.setMsg("标题不能为空");
+            return ajaxJson;
+        }
+        if (StringUtil.isEmpty(imgs)) {
+            ajaxJson.setCode(0);
+            ajaxJson.setMsg("图片不能为空");
+            return ajaxJson;
+        }
+        if (StringUtil.isEmpty(content)) {
+            ajaxJson.setCode(0);
+            ajaxJson.setMsg("内容不能为空");
+            return ajaxJson;
+        }
+        //资料添加
+        TrainingDynamic trainingDynamic = new TrainingDynamic();
+        trainingDynamic.setId(UUID.randomUUID().toString());
+        trainingDynamic.setImgs(imgs);
+        trainingDynamic.setContent(content);
+        trainingDynamic.setRemarks(remarks);
+        trainingDynamic.setCreateUserId(user.getId());
+        trainingDynamic.setCreateDate(new Date());
+        return ajaxJson;
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public AjaxJson delete(@PathVariable("ids") String ids) {
+        AjaxJson ajaxJson = new AjaxJson();
+
+        if (StringUtil.isEmpty(ids)){
+            ajaxJson.setCode(0);
+            ajaxJson.setMsg("删除失败，请先选择要删除的对象");
+            return ajaxJson;
+        }
+
+        boolean delete = trainingDynamicService.batchDelete(ids);
+        if (delete){
+            ajaxJson.setCode(1);
+            ajaxJson.setMsg("删除成功");
+        }else {
+            ajaxJson.setCode(0);
+            ajaxJson.setMsg("删除失败，请稍后重试");
+        }
+
+        return ajaxJson;
+    }
+}
---
+++ src/main/java/com/trainingcenter/service/impl/TrainingDynamicIServiceImpl.java
@@ -0,0 +1,144 @@
+package com.trainingcenter.service.impl;
+
+import com.trainingcenter.bean.TrainingDynamic;
+import com.trainingcenter.dao.TrainingDynamicMapper;
+import com.trainingcenter.exception.DeleteException;
+import com.trainingcenter.exception.FindException;
+import com.trainingcenter.service.TrainingDynamicService;
+import com.trainingcenter.utils.LogUtil;
+import com.trainingcenter.utils.StringUtil;
+import com.trainingcenter.utils.SysResourcesUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Liutingwei
+ * @date 2018-11-6 21:44
+ */
+@Service("trainingDynamicService")
+public class TrainingDynamicIServiceImpl implements TrainingDynamicService {
+    @Qualifier("trainingDynamicMapper")
+    @Autowired
+    private TrainingDynamicMapper trainingDynamicMapper;
+
+    /**
+     *通过id获取培训动态
+     * @param id：培训动态id
+     * @return 返回对应的培训动态对象
+     */
+    @Override
+    public TrainingDynamic getTrainingDynamicById(String id) {
+        return StringUtil.isEmpty(id)?null:trainingDynamicMapper.getTrainingDynamicById(id);
+    }
+
+    /**
+     *分页获取所以培训动态
+     * @param currentPage：当前页
+     * @param rows：每页要显示的数据条数
+     * @param searchContent：模糊查询内容
+     * @return
+     */
+    @Override
+    public List<TrainingDynamic> getTrainingDynamics(Integer currentPage, Integer rows, String searchContent) {
+            if(currentPage < 0 || rows < 0){
+                return null;
+            }
+            Integer start = (currentPage - 1) * rows;
+            return trainingDynamicMapper.getTrainingDynamics(start,rows,searchContent);
+    }
+
+    /**
+     * 培训动态添加方法
+     * @param trainingDynamic：要添加的培训动态
+     * @return 返回值大于0表示操作成功，否则操作失败
+     */
+    @Override
+    public Integer add(@Valid TrainingDynamic trainingDynamic) {
+        return trainingDynamic == null ? 0:trainingDynamicMapper.add(trainingDynamic);
+    }
+
+    /**
+     * 培训动态更新方法
+     * @param trainingDynamic:要更新的培训动态
+     * @return 返回值大于0表示操作成功，否则操作失败
+     */
+    @Override
+    public Integer update(@Valid TrainingDynamic trainingDynamic) {
+        return trainingDynamic == null ? 0:trainingDynamicMapper.update(trainingDynamic);
+    }
+
+    /**
+     * 培训动态删除方法
+     * @param id：培训动态id
+     * @return 返回值大于0表示操作成功，否则操作失败
+     * @throws DeleteException
+     */
+    @Override
+    public Integer delete(String id) throws DeleteException {
+        Integer result = 0;
+        if (StringUtil.isNotEmpty(id)){
+            if (this.getTrainingDynamicById(id) != null){//确保要删除对象存在
+                result = trainingDynamicMapper.delete(id);
+                if (result == 0){
+                    throw new DeleteException("删除失败，请稍后重试！");
+                }
+            }else {
+                throw new DeleteException("删除失败，动态培训不存在或已被删除！");
+            }
+        }else {
+            throw new DeleteException("删除失败，删除动态培训id不能为空！");
+        }
+        return result;
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param ids：需要删除的对象的id集
+     * @return 返回操作结果（true：删除成功，false：删除失败）
+     * 添加事务，保证中间删除失败时可以回滚
+     */
+    @Override
+    @Transactional
+    public boolean batchDelete(String ids) throws FindException, DeleteException {
+        if (StringUtil.isEmpty(ids))
+            return false;
+
+        //获取当前登录用户
+        String currentUsername = SysResourcesUtils.getCurrentUsername();
+        LogUtil.info(this, "角色批量删除", "用户：【" + currentUsername + "】正在批量删除IDS为：【" + ids + "】的角色");
+
+        String[] arr = ids.split(",");  //分割成数组
+        for (String id : arr) {
+            Integer res = this.delete(id);
+            if (res == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @param trainingDynamicIds：多个培训动态ID
+     * @return 返回培训类型对象集
+     */
+    @Override
+    public List<TrainingDynamic> getTrainingDynamicsByTrainingDynamics(String trainingDynamicIds) {
+        if (StringUtil.isEmpty(trainingDynamicIds))
+            return null;
+        List<TrainingDynamic> list = new ArrayList<>();
+        String[] arr = trainingDynamicIds.split(",");
+        for (String id : arr) {
+            TrainingDynamic trainingDynamic = this.getTrainingDynamicById(id);
+            list.add(trainingDynamic);
+        }
+        return list;
+    }
+}
