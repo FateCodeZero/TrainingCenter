0084d87a54142d8501567e6ca581e14fde2b6d25
LiuTingWed <1991261508@qq.com>
2018-12-04 22:45:20 +0800

    特色食物修复BUG

--- src/main/java/com/trainingcenter/controller/SpecialFoodController.java
+++ src/main/java/com/trainingcenter/controller/SpecialFoodController.java
@@ -1,10 +1,10 @@
 package com.trainingcenter.controller;
 
-import com.trainingcenter.bean.TrainingDynamic;
+import com.trainingcenter.bean.SpecialFood;
 import com.trainingcenter.bean.User;
 import com.trainingcenter.controller.validation.TC_Update;
 import com.trainingcenter.exception.UpdateException;
-import com.trainingcenter.service.TrainingDynamicService;
+import com.trainingcenter.service.SpecialFoodService;
 import com.trainingcenter.service.UserService;
 import com.trainingcenter.utils.AjaxJson;
 import com.trainingcenter.utils.StringUtil;
@@ -30,14 +30,14 @@ import static com.trainingcenter.utils.SysResourcesUtils.getCurrentUsername;
  * @author Liutingwei
  * @date 2018-11-9 19:45
  */
-@RequestMapping("/trainingDynamic")
+@RequestMapping("/specialFood")
 @Controller
 public class SpecialFoodController {
 
 
-    @Qualifier("trainingDynamicService")
+    @Qualifier("specialFoodService")
     @Autowired
-    private TrainingDynamicService trainingDynamicService;
+    private SpecialFoodService specialFoodService;
 
     @Qualifier("userService")
     @Autowired
@@ -51,10 +51,10 @@ public class SpecialFoodController {
             ajaxJson.setCode(0);
             ajaxJson.setMsg("参数异常");
             return ajaxJson;        }
-        List<TrainingDynamic> trainingDynamics = trainingDynamicService.getTrainingDynamics(currentPage, rows, searchContent);
-        Integer total = trainingDynamicService.getTrainingDynamics().size();
+        List<SpecialFood> specialFoods = specialFoodService.getSpecialFoods(currentPage, rows, searchContent);
+        Integer total = specialFoodService.getSpecialFoods().size();
 
-        if (trainingDynamics.size() == 0){
+        if (specialFoods.size() == 0){
             ajaxJson.setCode(0);
             ajaxJson.setMsg("暂无数据");
         }else {
@@ -64,7 +64,7 @@ public class SpecialFoodController {
 
         Map<String,Object> data = new ConcurrentHashMap<>();
         data.put("total",total);
-        data.put("items",trainingDynamics);
+        data.put("items",specialFoods);
         ajaxJson.setData(data);
         return ajaxJson;
     }
@@ -79,8 +79,8 @@ public class SpecialFoodController {
     public AjaxJson detailsPage(@RequestParam("id") String id) {
         AjaxJson ajaxJson = new AjaxJson();
         if(StringUtil.isNotEmpty(id)){
-            TrainingDynamic trainingDynamic = trainingDynamicService.getTrainingDynamicById(id);
-            if (trainingDynamic == null){
+            SpecialFood specialFood = specialFoodService.getSpecialFoodById(id);
+            if (specialFood == null){
                 ajaxJson.setCode(1);
                 ajaxJson.setMsg("暂无数据");
                 return ajaxJson;
@@ -88,7 +88,7 @@ public class SpecialFoodController {
                 ajaxJson.setCode(1);
                 ajaxJson.setMsg("請求成功");
                 Map<String, Object> map = new ConcurrentHashMap<>(); //返回携带的数据
-                map.put("items",trainingDynamic);
+                map.put("items",specialFood);
                 ajaxJson.setData(map);
                 return ajaxJson;
             }
@@ -105,8 +105,8 @@ public class SpecialFoodController {
      * @return
      */
     @ResponseBody
-    @RequestMapping("/getTrainingDynamicById")
-    public AjaxJson getTrainingDynamicById(@RequestParam("id") String id) {
+    @RequestMapping("/getSpecialFoodById")
+    public AjaxJson getSpecialFoodById(@RequestParam("id") String id) {
         AjaxJson ajaxJson = new AjaxJson();
         if (StringUtil.isEmpty(id)) {
             ajaxJson.setCode(0);
@@ -114,7 +114,7 @@ public class SpecialFoodController {
             return ajaxJson;
         }
 
-        TrainingDynamic resource = trainingDynamicService.getTrainingDynamicById(id);
+        SpecialFood resource = specialFoodService.getSpecialFoodById(id);
         if (resource == null) {
             ajaxJson.setCode(0);
             ajaxJson.setMsg("对象不存在或已被删除");
@@ -123,14 +123,14 @@ public class SpecialFoodController {
             ajaxJson.setCode(1);
             ajaxJson.setMsg("获取成功");
             Map<String, Object> map = new ConcurrentHashMap<>();
-            map.put("trainingDynamic", resource);
+            map.put("specialFood", resource);
             ajaxJson.setData(map);
             return ajaxJson;
         }
     }
     @RequestMapping(value = "/update",method = RequestMethod.GET)
     @ResponseBody
-    public AjaxJson update(@Validated(value = {TC_Update.class}) TrainingDynamic trainingDynamic){
+    public AjaxJson update(@Validated(value = {TC_Update.class}) SpecialFood specialFood){
         AjaxJson ajaxJson = new AjaxJson();
         Integer res;    //操作结果 flag
 
@@ -143,25 +143,25 @@ public class SpecialFoodController {
             throw new CredentialsExpiredException("登录凭证已过期");
         }
 
-        if (trainingDynamic == null){
+        if (specialFood == null){
             ajaxJson.setCode(0);
             ajaxJson.setMsg("操作失败，对象不能为空");
             return ajaxJson;
 
         }
         //更新操作
-        TrainingDynamic newTD = trainingDynamicService.getTrainingDynamicById(trainingDynamic.getId());
+        SpecialFood newTD = specialFoodService.getSpecialFoodById(specialFood.getId());
         if (newTD == null){
             throw new UpdateException("更新失败，对象不存在或已被删除");
         }
-        newTD.setTitle(trainingDynamic.getTitle());
-        newTD.setRemarks(trainingDynamic.getRemarks());
-        newTD.setContent(trainingDynamic.getContent());
-        newTD.setImgs(trainingDynamic.getImgs());
+        newTD.setTitle(specialFood.getTitle());
+        newTD.setRemarks(specialFood.getRemarks());
+        newTD.setContent(specialFood.getContent());
+        newTD.setImgs(specialFood.getImgs());
         newTD.setUpdateDate(new Date());
         newTD.setUpdateUserId(user.getId());
 
-        res=trainingDynamicService.update(newTD);
+        res=specialFoodService.update(newTD);
         if (res == 0){
             ajaxJson.setCode(0);
             ajaxJson.setMsg("操作失败，请重试");
@@ -200,14 +200,14 @@ public class SpecialFoodController {
             return ajaxJson;
         }
         //资料添加
-        TrainingDynamic trainingDynamic = new TrainingDynamic();
-        trainingDynamic.setId(UUID.randomUUID().toString());
-        trainingDynamic.setImgs(imgs);
-        trainingDynamic.setContent(content);
-        trainingDynamic.setRemarks(remarks);
-        trainingDynamic.setCreateUserId(user.getId());
-        trainingDynamic.setCreateDate(new Date());
-        Integer add = trainingDynamicService.add(trainingDynamic);
+        SpecialFood specialFood = new SpecialFood();
+        specialFood.setId(UUID.randomUUID().toString());
+        specialFood.setImgs(imgs);
+        specialFood.setContent(content);
+        specialFood.setRemarks(remarks);
+        specialFood.setCreateUserId(user.getId());
+        specialFood.setCreateDate(new Date());
+        Integer add = specialFoodService.add(specialFood);
         if (add==1){
             ajaxJson.setCode(1);
             ajaxJson.setMsg("添加成功");
@@ -229,7 +229,7 @@ public class SpecialFoodController {
             return ajaxJson;
         }
 
-        Integer delete = trainingDynamicService.batchDelete(ids);
+        Integer delete = specialFoodService.batchDelete(ids);
         if (delete==1){
             ajaxJson.setCode(1);
             ajaxJson.setMsg("删除成功");
